9e925f05dca127299e21b3e25078698c2339c62c
usamagithub123 <usamaashraf540@gmail.com>
2024-07-24 20:59:10 +0530

    2nd part completed -ui design

---
+++ frontend/src/components/messages/Message.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Message = () => {
+  return (
+    <div className='chat chat-end' >
+        <div className='chat-image avatar' >
+         <div className='w-10 rounded-full' >
+       
+
+         </div>
+        </div>
+        <div>
+            <div className={`chat-bubble text-white bg-blue-500`} >hi! what is up</div>
+            <div></div>
+        </div>
+    </div>
+  )
+}
+
+export default Message
