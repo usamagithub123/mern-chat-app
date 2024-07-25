ac7c51c4969fc61f11af0e10492d4a1e02c860fd
usamagithub123 <usamaashraf540@gmail.com>
2024-07-25 17:25:54 +0530

    Deployment

--- backend/server.js
+++ backend/server.js
@@ -2,12 +2,14 @@ import express from "express"
 import dotenv from "dotenv"
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
+import path from "path";
 import userRoutes from "./routes/user.routes.js";
 import cookieParser from "cookie-parser";
 import connectdb from "./db/connectdb.js";
 import {app,server} from "./socket/socket.js"
 
 const PORT=process.env.PORT || 5000
+const __dirname =path.resolve();
 dotenv.config();
 app.use(express.json());
 app.use(cookieParser());
@@ -15,9 +17,12 @@ app.use("/api/auth",authRoutes);
 app.use("/api/messages",messageRoutes);
 app.use("/api/users",userRoutes);
 
-// app.get("/",(req,res)=>{
-//     res.send("hello world");
-// });
+app.use(express.static(path.join(__dirname,"/frontend/dist")))
+
+app.get("*",(req,res)=>{
+    res.sendFile(path.join(__dirname,"frontend","dist","index.html"))
+})
+
 
 
 server.listen(PORT,()=>{
